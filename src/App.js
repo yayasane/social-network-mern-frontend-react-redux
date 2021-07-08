4ba8b436bddfd92430e54afb4d00e59c849eea18
yayaveli <yabadji2010@gmail.com>
2021-07-08 01:57:34 +0000

    Feat: Add Router navigation

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Routes from './components/Routes/index'
+const App = () => {
+  return (
+    <div>
+      <Routes />
+    </div>
+  )
+}
+
+export default App
---
+++ src/pages/Profil.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Profil = () => {
+  return <div>Voir le profil</div>
+}
+
+export default Profil
